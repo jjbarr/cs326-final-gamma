3c4a2436f4c6a66bd29494e8fe568bf341aa69a5
Ry-Wu <55855499+Ry-Wu@users.noreply.github.com>
2021-10-22 19:30:00 -0400

    user interface improved

---
+++ client/user-interface.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', ()=>{   
+    document.getElementById('return-btn').addEventListener('click', returnHome);
+});
+
+function returnHome(){
+    location.href = "./index.html?login=true";
+}
