ead7036fe529018204f4221c925163306b769aee
Ry-Wu <55855499+Ry-Wu@users.noreply.github.com>
2021-11-06 05:14:13 -0400

    user input and storage

--- client/user-interface.js
+++ client/user-interface.js
@@ -1,11 +1,34 @@
 window.addEventListener('DOMContentLoaded', ()=>{   
+    document.getElementById('remove-btn').addEventListener('click', deleteReview);
     document.getElementById('return-btn').addEventListener('click', returnHome);
+    document.getElementById('add-btn').addEventListener('click', updateReview);
 });
 
 function returnHome(){
     location.href = "./index.html?login=true";
 }
 
-function update(){
-    
+function deleteReview() {
+    document.getElementById('row').remove();
 }
+  
+function updateReview() {
+    let x = document.getElementById('userReview');
+    let newRow = x.rows[1].cloneNode(true);
+    let length = x.rows.length;
+  
+    let lm = newRow.cells[0].getElementsByTagName('input')[0];
+    lm.id += length;
+    lm.value = '';
+
+    let rev = newRow.cells[1].getElementsByTagName('input')[0];
+    rev.id += length;
+    rev.value = '';
+    
+    let rate = newRow.cells[2].getElementsByTagName('input')[0];
+    rate.id += length;
+    rate.value = '';
+
+    x.appendChild(newRow);
+}
+
--- server/server.js
+++ server/server.js
@@ -6,16 +6,21 @@ app.use(express.static('client'));
 app.use(express.urlencoded({extended:true}));
 
 let userJSONfile = 'userInfo.json';
+let reviewJSONfile = 'userReviews.json';
 
 //home page: once open localhost:3000
 app.get('/', (req, res) => {
     res.redirect('index.html');
 })
 
+//redirect to login page
 app.get('/login', (req, res) => {
     res.redirect('login.html');
 });
 
+/* Check if user already signed up. If not, ask user to sign up.
+ * If user already has an account, check if the password matches that in the file.
+ */
 app.post('/login', (req, res) => {
     let userFile = JSON.parse(fs.readFileSync(userJSONfile));
     let body = req.body;
@@ -38,14 +43,17 @@ app.post('/login', (req, res) => {
     }
 });
 
+//once log out, redirect to the homepage
 app.post('/logout', (req, res) => {
     res.redirect('/');
 });
 
+//redirect to the signup page
 app.get('/signup', (req, res) => {
     res.redirect('signup.html');
 });
 
+//user put in username and passwords to sign up, and save user info in a JSON file
 app.post('/signup', (req, res) => {
     let body = req.body;
     let username = body['username'];
@@ -71,24 +79,47 @@ app.get('/landmark/:id', (req, res) => {});
 app.patch('/landmark/:id', (req,res) => {});
 app.delete('/landmark/:id', (req,res) => {});
 app.post('/landmark/:id/add_review', (req,res) => {});
-app.get('/review/:id', (req,res) => {});
 app.patch('/review/:id', (req, res) => {});
-app.delete('/review/:id', (req,res) => {});
 app.get('/user/:id', (req,res) => {});
 app.get('/landmarks_in', (req, res) => {});
 
+//redirect to review page and show all the reviews
+app.get('/review/:id', (req,res) => {
+    res.redirect('/user-interface.html');
+    // let reviews =[];
+    // if (fs.existsSync(reviewJSONfile)) {
+    //     reviews = JSON.parse(fs.readFileSync(reviewJSONfile));
+    // }
+    // console.log(reviews);
+    // for(let i = 0 ; i < reviews.length; i++){
+    //     reviews['name']
+    // }
+});
 
-// app.get('/crud', (req, res)=>{
-//     res.redirect('crud.html');
-// });
+//save all reviews to a JSON file
+app.post('/review/:id', (req,res) => {
+    let body = req.body;
+    let landmark = body['name'];
+    let review = body["review"];
+    let rating = body["rating"];
+ 
+    let userReviews = [];
+    if (fs.existsSync(reviewJSONfile)) {
+        let someStr = fs.readFileSync(reviewJSONfile);
+        userReviews = JSON.parse(someStr);
+    }
+    let userReview = {};
+    userReview['name'] = landmark;
+    userReview['review'] = review;
+    userReview['rating'] = rating;
+    userReviews.push(userReview);
+    fs.writeFileSync(reviewJSONfile, JSON.stringify(userReviews));
+});
 
-// app.get('/crud/data', (req, res)=>{
-    
-// });
+//delete a review from the JSON file
+app.delete('/review/:id', (req,res) => {
+});
 
-// app.post('/crud/data', (req, res)=>{
-//     res.json({'request': 'post'});
-// });
 
 //port
 const port = process.env.PORT || 3000;
